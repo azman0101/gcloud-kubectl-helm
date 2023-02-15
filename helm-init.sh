9faef44871b2a23535d9e08060c39e26c3bda819
jboulanger <julien.boulanger@fr.clara.net>
2023-02-15 13:30:51 +0100

    chore: refacto version

--- helm-init.sh
+++ helm-init.sh
@@ -5,15 +5,15 @@ set -x
 
 if helm version --client | grep -q 'SemVer:"v2';then
     helm init --client-only
-    helm plugin install https://github.com/hayorov/helm-gcs.git --version 0.4.1
-    helm plugin install https://github.com/databus23/helm-diff --version 3.6.0
+    helm plugin install https://github.com/hayorov/helm-gcs.git --version "${HELM_GCS_VERSION}"
+    helm plugin install https://github.com/databus23/helm-diff --version "${HELM_DIFF_VERSION}"
     helm plugin list
 else
     helm repo add stable https://charts.helm.sh/stable
-    helm plugin install https://github.com/jkroepke/helm-secrets --version v4.2.2
-    helm plugin install https://github.com/hayorov/helm-gcs.git --version 0.4.1
-    helm plugin install https://github.com/databus23/helm-diff --version 3.6.0
-    helm plugin install https://github.com/helm/helm-mapkubeapis --version 0.3.2
+    helm plugin install https://github.com/jkroepke/helm-secrets --version "${HELM_SECRETS_VERSION}"
+    helm plugin install https://github.com/hayorov/helm-gcs.git --version "${HELM_GCS_VERSION}"
+    helm plugin install https://github.com/databus23/helm-diff --version "${HELM_DIFF_VERSION}"
+    helm plugin install https://github.com/helm/helm-mapkubeapis --version "${HELM_MAPKUBEAPIS_VERSION}"
     helm plugin list
 fi
 
